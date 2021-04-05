9c10279d8a1dfa9cb92d556c25a6299b2395dfb1
Adith Raghav <65845669+adithraghavs@users.noreply.github.com>
2021-04-05 17:36:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.get('/', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
