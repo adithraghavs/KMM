3ea539c2ea8df376cdce75b00220c7a4b9f95a4d
adithraghavs <65845669+adithraghavs@users.noreply.github.com>
2021-04-05 16:58:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send(JSON.stringify({code: "function test() {console.log('Hola Amigo, Ciao Amico!')}"}))
+});
+
+app.listen(port)
